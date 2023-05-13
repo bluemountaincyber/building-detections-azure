b2ceed037120dc2e47073f8841a19a8e38462d48
Cyb3rB3ar <alexander.braulik@googlemail.com>
2023-05-14 01:05:16 +0200

    added reader role to current user for blob storage

--- terraform/users.tf
+++ terraform/users.tf
@@ -18,3 +18,9 @@ resource "azurerm_role_assignment" "mhooper_storage_read" {
   role_definition_name = "Reader and Data Access"
   principal_id         = azuread_service_principal.storage_manager_sp.id
 }
+
+resource "azurerm_role_assignment" "current_user_storage_read" {
+  scope                = azurerm_storage_account.prod_data.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = data.azuread_client_config.current.object_id
+}
