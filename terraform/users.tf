3504e8950161fd193dfd0f2624ec7fd691c8ced6
Cyb3rB3ar <alexander.braulik@googlemail.com>
2023-05-14 02:36:01 +0200

    added missing Reader role for service principal to storage account

--- terraform/users.tf
+++ terraform/users.tf
@@ -19,6 +19,12 @@ resource "azurerm_role_assignment" "store_manager_storage_blob_data_reader" {
   principal_id         = azuread_service_principal.storage_manager_sp.id
 }
 
+resource "azurerm_role_assignment" "store_manager_storage_account_reader" {
+  scope                = azurerm_storage_account.prod_data.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.storage_manager_sp.id
+}
+
 resource "azurerm_role_assignment" "current_user_storage_blob_data_reader" {
   scope                = azurerm_storage_account.prod_data.id
   role_definition_name = "Storage Blob Data Reader"
